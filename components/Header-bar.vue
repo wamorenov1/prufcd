68d82fd4b75a6798b7b29bf71d273dbd182ee566
William Moreno <wamoreno1@gmail.com>
2022-08-31 20:46:19 -0500

    Añadido, assets fijos componentes y primer layout

---
+++ components/Header-bar.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header">
+      <div class="container">
+        <nav class="nav__container">
+          <div class="nav__logo bfix"><img src="" alt=""></div>
+          <div class="nav__button bfix">
+            <div class="login-button">
+              <div class="login-button__letter flex-center">
+                <p>W</p>
+              </div><a href=""> 
+                <div class="login-button__dropdown"><img src="" aria-label="Desplegar menú"></div></a>
+              <div class="login-button__status"></div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Header-bar></Header-bar>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
